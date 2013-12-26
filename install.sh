3965888d8da8575357a48f4d64076e0e1a063393
Stephan Hilb <stephan@ecshi.net>
2013-12-26 15:25:45 +0100

    install: provide some feedback

--- install.sh
+++ install.sh
@@ -5,18 +5,24 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 
 # Vim
 if [ ! -e ~/.vim ]; then
+	echo "Installing vim dotfiles."
 	ln -s $DIR/vim           ~/.vim
 	ln -s $DIR/vim/vimrc     ~/.vimrc
 	git clone https://github.com/gmarik/vundle.git $DIR/vim/bundle/vundle
 	vim -c "BundleInstall" -c ":qa"
+else
+	echo "Found '~/.vim', not doing anything."
 fi
 
 # Git
 if [ ! -e ~/.gitconfig ]; then
+	echo "Installing git dotfiles."
 	# before any "git config"
 	ln -s $DIR/git/gitconfig ~/.gitconfig
 
 	ln -s $DIR/git/gitignore ~/.gitignore
 	git config --global core.excludesfile ~/.gitignore
+else
+	echo "Found '~/.gitconfig', not doing anything."
 fi
 
