24e8b2063a540125ff377949b21542da200a5397
Stephan Hilb <stephan.hilb@mathematik.uni-stuttgart.de>
2019-05-01 07:40:51 +0200

    new status bar

---
+++ bin/mybar.jl
@@ -0,0 +1,36 @@
+import JSON.json
+using Dates
+
+function init(;kwargs...)
+    println(json(kwargs))
+    println("[")
+end
+
+function step(blockfns...)
+    println(json([f() for f in blockfns]), ",")
+end
+
+@generated function dateformat()
+    f = DateFormat("Y-mm-dd e HH:MM:SS")
+    return :($f)
+end
+
+function date()
+    nowtime = now()
+    format = dateformat()
+    return (full_text = Dates.format(nowtime, format),)
+end
+
+function battery()
+    path = "/sys/class/power_supply/BAT0"
+    cap = parse(Int, readline(joinpath(path, "capacity")))
+    return (full_text = "$(cap)%",)
+end
+
+init(version = 1, click_events = true)
+
+while true
+    step(battery, date)
+    sleep(1)
+end
+
