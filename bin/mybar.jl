631f107edee413fc7e3e02c204cc634531415338
Stephan Hilb <stephan.hilb@mathematik.uni-stuttgart.de>
2019-06-14 11:06:46 +0200

    mybar: add battery discharge rate

--- bin/mybar.jl
+++ bin/mybar.jl
@@ -30,7 +30,17 @@ function battery()
     cap = parse(Int, readline(joinpath(path, "capacity")))
     status = readline(joinpath(path, "status"))
 
-    return (full_text = @sprintf("%s%% %s", cap, status),)
+    if status == "Discharging" && !isempty(readline(joinpath(path, "current_now")))
+        current_now = parse(Int, readline(joinpath(path, "current_now"))) / 10^6
+        voltage_now = parse(Int, readline(joinpath(path, "voltage_now"))) / 10^6
+        charge_now = parse(Int, readline(joinpath(path, "charge_now")))
+
+        discharge = current_now * voltage_now
+
+        return (full_text = @sprintf("%s%% %s at %4.2fW", cap, status, discharge),)
+    else
+        return (full_text = @sprintf("%s%% %s", cap, status),)
+    end
 end
 
 init(version = 1, click_events = true)
