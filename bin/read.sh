fe4b1b352c004ca998e83008cb0392be81764f3e
Stephan Hilb <stephan@ecshi.net>
2019-12-12 22:58:58 +0100

    move to bemenu

--- bin/read.sh
+++ bin/read.sh
@@ -11,6 +11,6 @@ list() {
   done
 }
 
-selection="$(list | dmenu -i -l 5)"
+selection="$(list | bemenu -l 5 --nf '#eff0eb' --hb '#78787e' --hf '#eff0eb' --tf '#eff0eb' --ff '#57c7ff' --fn 'DejaVu Sans Mono 14' -p '')"
 
 [ -z "$selection" ] || zathura "$selection"
