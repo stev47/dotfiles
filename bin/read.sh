a199f3ec4e893c8fd602374d0ed4160f106aa09d
Stephan Hilb <stephan.hilb@mathematik.uni-stuttgart.de>
2019-12-13 16:44:51 +0100

    bin/read: ignore case

--- bin/read.sh
+++ bin/read.sh
@@ -11,6 +11,6 @@ list() {
   done
 }
 
-selection="$(list | bemenu -l 5 --nf '#eff0eb' --hb '#78787e' --hf '#eff0eb' --tf '#eff0eb' --ff '#57c7ff' --fn 'DejaVu Sans Mono 14' -p '')"
+selection="$(list | bemenu -i -l 5 --nf '#eff0eb' --hb '#78787e' --hf '#eff0eb' --tf '#eff0eb' --ff '#57c7ff' --fn 'DejaVu Sans Mono 14' -p '')"
 
 [ -z "$selection" ] || zathura "$selection"
